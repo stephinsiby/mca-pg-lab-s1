3afe8eb9e20196ec77909ea27455569e58885e49
stephin siby <84508020+stephinsiby@users.noreply.github.com>
2025-09-11 15:24:22 +0530

    Add files via upload

---
+++ stephinpg2.py
@@ -0,0 +1,3 @@
+s = input("enter the sring:")
+new=s[-1] + s[1:-1] + s[0]
+print("the exchanged string is:",new)
---
+++ stephinpg3.py
@@ -0,0 +1,3 @@
+s = input("enter the string:")
+new=s[-1] + s[1:-1] + s[0]
+print("the exchanged string is:",new)
